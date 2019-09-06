cb93f33ab794badaecc48bd0300aa9ec5577407a
Andrew Young <andrew@youngshome.com>
2019-09-06 14:05:44 -0600

    Edit - Server Change

--- to-do-personal/vue.config.js
+++ to-do-personal/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {
         '^/api': {
-          target: 'http://localhost:3003',
+          target: 'http://localhost:3000',
         },
       }
     }
